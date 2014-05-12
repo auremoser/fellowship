385c65e1d3341759a927408ff858d654ba16694c
Aurelia Moser <auremoser@gmail.com>
2014-05-12 10:00:56 -0400

    update the python scripts to run chambua

--- projects/[IN]HIV/send_json.py
+++ projects/[IN]HIV/send_json.py
@@ -6,27 +6,33 @@ import sys
 import traceback
 import argparse
 import glob
-import json
+import urllib2
+import os
+
+
+def save_file(content, filename, dir):
+    filename = os.path.join(dir, os.path.basename(filename))
+    target = open(filename, 'w')
+    target.write(content)
+    target.close()
 
 
 def main():
     parser = argparse.ArgumentParser(description='Wrap file content in json')
     parser.add_argument('-p', '--pattern', default='./*.txt', help='foo')
+    parser.add_argument('-o', '--output', default='./output', help='foo')
     args = parser.parse_args()
 
-    fix_encoding = lambda s: s.encode('ascii', 'ignore')
-
+    url = 'http://chambua.ushahidi.com/v1/tags'
     for file in glob.glob(args.pattern):
         with open(file, "r+") as f:
-            # content = fix_encoding(f.read())
-            content = f.read()
-            content = content.rstrip('\r\n')
-            content = json.dumps({'text': content}, ensure_ascii=True)
-            f.seek(0)
-            try:
-                f.write((content))
-            except Exception, e:
-                print e
+            data = f.read()
+            req = urllib2.Request(url, data, {'Content-Type': 'application/json'})
+            rq = urllib2.urlopen(req)
+            response = rq.read()
+            rq.close()
+            save_file(response, file, args.output)
+        
 
 if __name__ == '__main__':
     try:
